5e83f11717c9e00916e10620aeb4bbf580634eed
bma <baptiste.marechal@femto-st.fr>
2018-01-24 08:46:13 +0100

    redpitaya/gain_only: update

--- gain_only/gain_only.runs/impl_1/rundef.js
+++ gain_only/gain_only.runs/impl_1/rundef.js
@@ -27,7 +27,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "init_design", "begin" );
+ISETouchFile( "write_bitstream", "begin" );
 ISEStep( "vivado",
          "-log gain_only_wrapper.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source gain_only_wrapper.tcl -notrace" );
 
--- gain_only/gain_only.runs/impl_1/runme.sh
+++ gain_only/gain_only.runs/impl_1/runme.sh
@@ -37,7 +37,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log gain_only_wrapper.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source gain_only_wrapper.tcl -notrace
 
 
