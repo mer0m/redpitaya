eaecdbb6560e3a46dd8193142d040f01e2780cde
bma <baptiste.marechal@femto-st.fr>
2018-05-09 16:34:45 +0200

    legacy

--- pid_vco_amp_mod_pid_only/app/pid_vco_amp_mod_pid_only_us.sh
+++ pid_vco_amp_mod_pid_only/app/pid_vco_amp_mod_pid_only_us.sh
@@ -1,6 +1,5 @@
 CORE_MODULES_DIR=/usr/local/modules
-cp ../bitstreams/pid_vco_amp_mod_pid_only_wrapper.bit.bin /lib/firmware
-echo "pid_vco_amp_mod_pid_only_wrapper.bit.bin" > /sys/class/fpga_manager/fpga0/firmware 
+cat ../bitstreams/pid_vco_amp_mod_pid_only_wrapper.bit > /dev/xdevcfg
 insmod ../modules/board_pid_vco_amp_mod_pid_only.ko
 insmod ${CORE_MODULES_DIR}/add_const_core.ko
 insmod ${CORE_MODULES_DIR}/nco_counter_core.ko
