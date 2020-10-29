44a83c841f5083ac524574410db271f0c9125f82
bma <bamarechal@free.fr>
2020-10-29 18:10:58 +0100

    square for fir delay meas

--- nco_fir_dac_adc/app/zmq_stream.py
+++ nco_fir_dac_adc/app/zmq_stream.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Zmq Stream
-# Generated: Sun Oct 25 00:17:01 2020
+# Generated: Thu Oct 29 18:06:15 2020
 ##################################################
 
 from distutils.version import StrictVersion
@@ -70,58 +70,56 @@ class zmq_stream(gr.top_block, Qt.QWidget):
         ##################################################
         self.zeromq_sub_source_0_0 = zeromq.sub_source(gr.sizeof_short, 1, 'tcp://192.168.0.200:9902', 200, False, -1)
         self.zeromq_sub_source_0 = zeromq.sub_source(gr.sizeof_short, 1, 'tcp://192.168.0.200:9901', 200, False, -1)
-        self.qtgui_sink_x_0_0 = qtgui.sink_c(
-        	8192, #fftsize
-        	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	0, #fc
-        	samp_rate, #bw
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+        	1024, #size
+        	samp_rate, #samp_rate
         	"", #name
-        	True, #plotfreq
-        	True, #plotwaterfall
-        	True, #plottime
-        	True, #plotconst
+        	6 #number of inputs
         )
-        self.qtgui_sink_x_0_0.set_update_time(1.0/1)
-        self._qtgui_sink_x_0_0_win = sip.wrapinstance(self.qtgui_sink_x_0_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_sink_x_0_0_win, 1, 0, 1, 1)
-        for r in range(1, 2):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
-            self.top_grid_layout.setColumnStretch(c, 1)
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
 
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
 
-        self.qtgui_sink_x_0_0.enable_rf_freq(False)
+        self.qtgui_time_sink_x_0.enable_tags(-1, True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(True)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
 
+        if not True:
+          self.qtgui_time_sink_x_0.disable_legend()
 
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "blue"]
+        styles = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+                   -1, -1, -1, -1, -1]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
 
-        self.qtgui_sink_x_0 = qtgui.sink_c(
-        	8192, #fftsize
-        	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	0, #fc
-        	samp_rate, #bw
-        	"", #name
-        	True, #plotfreq
-        	True, #plotwaterfall
-        	True, #plottime
-        	True, #plotconst
-        )
-        self.qtgui_sink_x_0.set_update_time(1.0/1)
-        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_sink_x_0_win, 0, 0, 1, 1)
-        for r in range(0, 1):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
-            self.top_grid_layout.setColumnStretch(c, 1)
+        for i in xrange(6):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
 
-
-        self.qtgui_sink_x_0.enable_rf_freq(False)
-
-
-
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
         self.blocks_short_to_float_0_0 = blocks.short_to_float(1, 1)
         self.blocks_short_to_float_0 = blocks.short_to_float(1, 1)
-        self.blocks_float_to_complex_0_0 = blocks.float_to_complex(1)
-        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
         self.blocks_deinterleave_0_0 = blocks.deinterleave(gr.sizeof_float*1, 1)
         self.blocks_deinterleave_0 = blocks.deinterleave(gr.sizeof_float*1, 1)
 
@@ -130,12 +128,12 @@ class zmq_stream(gr.top_block, Qt.QWidget):
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_deinterleave_0, 0), (self.blocks_float_to_complex_0, 0))
-        self.connect((self.blocks_deinterleave_0, 1), (self.blocks_float_to_complex_0, 1))
-        self.connect((self.blocks_deinterleave_0_0, 0), (self.blocks_float_to_complex_0_0, 0))
-        self.connect((self.blocks_deinterleave_0_0, 1), (self.blocks_float_to_complex_0_0, 1))
-        self.connect((self.blocks_float_to_complex_0, 0), (self.qtgui_sink_x_0, 0))
-        self.connect((self.blocks_float_to_complex_0_0, 0), (self.qtgui_sink_x_0_0, 0))
+        self.connect((self.blocks_deinterleave_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_deinterleave_0, 1), (self.qtgui_time_sink_x_0, 1))
+        self.connect((self.blocks_deinterleave_0, 2), (self.qtgui_time_sink_x_0, 2))
+        self.connect((self.blocks_deinterleave_0_0, 0), (self.qtgui_time_sink_x_0, 3))
+        self.connect((self.blocks_deinterleave_0_0, 1), (self.qtgui_time_sink_x_0, 4))
+        self.connect((self.blocks_deinterleave_0_0, 2), (self.qtgui_time_sink_x_0, 5))
         self.connect((self.blocks_short_to_float_0, 0), (self.blocks_deinterleave_0, 0))
         self.connect((self.blocks_short_to_float_0_0, 0), (self.blocks_deinterleave_0_0, 0))
         self.connect((self.zeromq_sub_source_0, 0), (self.blocks_short_to_float_0, 0))
@@ -151,8 +149,7 @@ class zmq_stream(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.qtgui_sink_x_0_0.set_frequency_range(0, self.samp_rate)
-        self.qtgui_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
 
 def main(top_block_cls=zmq_stream, options=None):
--- nco_fir_dac_adc/design/nco_fir_dac_adc.tcl
+++ nco_fir_dac_adc/design/nco_fir_dac_adc.tcl
@@ -3,13 +3,13 @@ add_ip_and_conf redpitaya_converters redpitaya_converters_0 {
 	CLOCK_DUTY_CYCLE_STABILIZER_EN false }
 connect_to_fpga_pins redpitaya_converters_0 phys_interface phys_interface_0
 
-############################################
+#############################################
 #											#
-#			 *	   *						#
+#				*		*					#
 # nco1 -> C2R -> fir1 -> DACA  ADCA -> *	#
 #											#
 #											#
-#			 *	   *				  		#
+#				*		*			  		#
 # nco2 -> C2R -> fir2 -> DACB  ADCB -> *	#
 #											#
 #############################################
@@ -24,19 +24,25 @@ connect_intf redpitaya_converters_0 rst_o nco1 ref_rst_i
 
 ## Create C2R1
 add_ip_and_conf convertComplexToReal c2r1 {
-	DATA_SIZE 16 }
-connect_intf nco1 sine_out c2r1 data_in
+	DATA_SIZE 1 }
+connect_intf nco1 square_out c2r1 data_in
+
+## Create instance: shifter_square1, and set properties
+add_ip_and_conf shifterReal shifter_square1 {
+	DATA_IN_SIZE 1 \
+	DATA_OUT_SIZE 16}
+connect_intf c2r1 dataI_out shifter_square1 data_in
 
 ## Create nco1_duppl
 add_ip_and_conf dupplReal nco1_duppl {
-	DATA_SIZE 14 \
+	DATA_SIZE 16 \
 	NB_OUTPUT 2}
 
-connect_intf c2r1 dataI_out nco1_duppl data_in
+connect_intf shifter_square1 data_out nco1_duppl data_in
 
 ## Create instance: fir1, and set properties
 add_ip_and_conf firReal fir1 {
-	DATA_IN_SIZE 14 \
+	DATA_IN_SIZE 16 \
 	DATA_OUT_SIZE 32 \
 	DECIMATE_FACTOR 1 \
 	NB_COEFF 25 }
@@ -47,16 +53,18 @@ connect_intf nco1_duppl data1_out fir1 data_in
 ## Create instance: shifter_fir1, and set properties
 add_ip_and_conf shifterReal shifter_fir1 {
 	DATA_IN_SIZE 32 \
-	DATA_OUT_SIZE 14}
+	DATA_OUT_SIZE 16}
 connect_intf fir1 data_out shifter_fir1 data_in
 
 ## Create fir1_out_duppl
 add_ip_and_conf dupplReal fir1_out_duppl {
-	DATA_SIZE 14 \
+	DATA_SIZE 16 \
 	NB_OUTPUT 2}
 
 connect_intf shifter_fir1 data_out fir1_out_duppl data_in
 #connect_intf fir1_out_duppl data1_out redpitaya_converters_0 dataA_in
+
+########################################################################
 
 ## Create nco2
 add_ip_and_conf nco_counter nco2 {
@@ -73,15 +81,15 @@ connect_intf nco2 sine_out c2r2 data_in
 
 ## Create nco2_duppl
 add_ip_and_conf dupplReal nco2_duppl {
-	DATA_SIZE 14 \
+	DATA_SIZE 16 \
 	NB_OUTPUT 2}
 
 connect_intf c2r2 dataI_out nco2_duppl data_in
 
 ## Create instance: fir2, and set properties
 add_ip_and_conf firReal fir2 {
-	DATA_IN_SIZE 14 \
-	DATA_OUT_SIZE 14 \
+	DATA_IN_SIZE 16 \
+	DATA_OUT_SIZE 32 \
 	DECIMATE_FACTOR 1 \
 	NB_COEFF 25 }
 connect_proc fir2 s00_axi 0x30000
@@ -91,36 +99,40 @@ connect_intf nco2_duppl data1_out fir2 data_in
 ## Create instance: shifter_fir2, and set properties
 add_ip_and_conf shifterReal shifter_fir2 {
 	DATA_IN_SIZE 32 \
-	DATA_OUT_SIZE 14}
+	DATA_OUT_SIZE 16}
 connect_intf fir2 data_out shifter_fir2 data_in
 
 ## Create fir2_out_duppl
 add_ip_and_conf dupplReal fir2_out_duppl {
-	DATA_SIZE 14 \
+	DATA_SIZE 16 \
 	NB_OUTPUT 2}
 
 connect_intf shifter_fir2 data_out fir2_out_duppl data_in
 #connect_intf fir2_out_duppl data1_out redpitaya_converters_0 dataB_in
 
+########################################################################
+
 ## Create instance: data1, and set properties
 add_ip_and_conf dataReal_to_ram data1 {
 	DATA_SIZE 16 \
-	NB_INPUT 2 \
+	NB_INPUT 3 \
 	NB_SAMPLE 8192 }
 connect_proc data1 s00_axi 0x40000
 
 connect_intf nco1_duppl data2_out data1 data1_in
 connect_intf fir1_out_duppl data2_out data1 data2_in
+connect_intf redpitaya_converters_0 dataA_out data1 data3_in
 
 ## Create instance: data2, and set properties
 add_ip_and_conf dataReal_to_ram data2 {
 	DATA_SIZE 16 \
-	NB_INPUT 2 \
+	NB_INPUT 3 \
 	NB_SAMPLE 8192 }
 connect_proc data2 s00_axi 0x50000
 
 connect_intf nco2_duppl data2_out data2 data1_in
 connect_intf fir2_out_duppl data2_out data2 data2_in
+connect_intf redpitaya_converters_0 dataB_out data2 data3_in
 
 ## ADC reset
 connect_proc_rst redpitaya_converters_0 adc_rst_i
