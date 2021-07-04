6669f05e56efa83e57dfbda973032c29916ece95
bma <bamarechal@free.fr>
2021-07-04 14:11:58 +0200

    fft: add app

---
+++ adc_fft/app/adc_fft_us.sh
@@ -0,0 +1,15 @@
+CORE_MODULES_DIR=../../modules
+
+mkdir -p /lib/firmware
+cp ../bitstreams/adc_fft_wrapper.bit.bin /lib/firmware
+DTB_DIR=/sys/kernel/config/device-tree/overlays/fpga
+if [ -d $DTB_DIR ]; then
+	rmdir $DTB_DIR
+fi
+mkdir $DTB_DIR
+cat adc_fft.dtbo > $DTB_DIR/dtbo
+
+insmod ${CORE_MODULES_DIR}/data_to_ram_core.ko
+insmod ${CORE_MODULES_DIR}/data_to_ram_core.ko
+insmod ${CORE_MODULES_DIR}/fft_core.ko
+insmod ${CORE_MODULES_DIR}/fir_core.ko
---
+++ adc_fft/app/adc_fft_webserver.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import liboscimp_fpga
+import ctypes
+import os
+import time
+from lxml import objectify
+import lxml.etree
+import remi.gui as gui
+from remi import start, App
+
+#Sampling frequency
+samp_freq = 125000000
+
+vals = objectify.Element("item")
+vals.config = "adc_fft_defconf.xml"
+
+class MyApp(App):
+	def __init__(self, *args):
+		super(MyApp, self).__init__(*args)
+
+	def main(self):
+		self.w = gui.VBox()
+
+		self.hbox_save_load = gui.HBox(margin="10px")
+		self.dtext_conf_file = gui.TextInput(width=200, height=30)
+		self.dtext_conf_file.set_value(str(vals.config))
+		self.dtext_conf_file.onchange.do(self.dtext_conf_file_changed)
+		self.bt_load = gui.Button("Load", width=200, height=30, margin="10px")
+		self.bt_load.onclick(self.bt_load_changed)
+		self.bt_save = gui.Button("Save", width=200, height=30, margin="10px")
+		self.bt_save.onclick(self.bt_save_changed)
+		self.hbox_save_load.append(self.dtext_conf_file)
+		self.hbox_save_load.append(self.bt_load)
+		self.hbox_save_load.append(self.bt_save)
+		self.w.append(self.hbox_save_load)
+
+		return self.w
+
+	def dtext_conf_file_changed(self, widget, value):
+		print(value)
+		vals.config=value
+
+	def bt_load_changed(self, widget):
+		with open(str(vals.config), "r") as f:
+			 lf = objectify.fromstring(f.read())
+
+		print("Configuration loaded")
+
+	def bt_save_changed(self, widget):
+		try:
+			os.remove(str(vals.config))
+		except:
+			pass
+		with open(str(vals.config), "wb") as f:
+			f.write(lxml.etree.tostring(vals, pretty_print=True))
+		print("Saved")
+
+start(MyApp, address="0.0.0.0", port=80, title="adc_fft_webserver")
