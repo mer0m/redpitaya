6a63a25a8f6b0a5a9fcfea9a064b77ba52cf9f11
bma <bamarechal@free.fr>
2021-07-10 19:21:36 +0200

    update fft coeff generator

--- adc_fft/app/fftGen.m
+++ adc_fft/app/fftGen.m
@@ -1,11 +1,13 @@
-nbcoeff = 2**11;
-coef_size = 14;
+#!/usr/bin/octave-cli
 
-IM(1:nbcoeff/2)=sin(linspace(0,nbcoeff/2,nbcoeff/2)/nbcoeff*2*pi)*2**(coef_size-1);
-IM(nbcoeff/2+1:nbcoeff)=0;
+nb_coeff = 2**eval(argv(){1}) ; % here 2048 is an example for 2**COEFF_ADDR_SIZE
+coeff_size = eval(argv(){2}) ; % COEFF_SIZE
 
-RE(1:nbcoeff/2)=cos(linspace(0,nbcoeff/2,nbcoeff/2)/nbcoeff*2*pi)*2**(coef_size-1);
-RE(nbcoeff/2+1:nbcoeff)=0;
+IM(1:nb_coeff/2)=sin(linspace(0,nb_coeff/2,nb_coeff/2)/nb_coeff*2*pi)*2**(coeff_size-1);
+IM(nb_coeff/2+1:nb_coeff)=0;
+
+RE(1:nb_coeff/2)=cos(linspace(0,nb_coeff/2,nb_coeff/2)/nb_coeff*2*pi)*2**(coeff_size-1);
+RE(nb_coeff/2+1:nb_coeff)=0;
 
 csvwrite('fft_im.dat', round(transpose(IM)))
 csvwrite('fft_re.dat', round(transpose(RE)))
